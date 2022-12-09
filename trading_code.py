339b7174be5977a2fb777c6ec4abfe591a0b870f
SIDERODROMOPHOBE <salibaa.gabriel@gmail.com>
2022-12-09 16:45:18 +0100

    Added getBid function

--- trading_code.py
+++ trading_code.py
@@ -3,3 +3,10 @@ import urllib.request as ur
 def getAllTokenPrice():
     url=ur.urlopen("https://api.binance.com/api/v3/ticker/price")
     print(str(url.read()))
+
+
+
+def getBid(symbol):
+    print(str(ur.urlopen("https://api.binance.com/api/v3/depth?symbol="+str(symbol)+"&limit=1").read()))
+
+getBid("BNBBTC")
